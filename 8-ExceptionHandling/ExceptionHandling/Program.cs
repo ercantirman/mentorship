64e8c51a586fabb18859e641826a89e1392a9934
Ercan Tırman <ercan.tirman@arfitect.com>
2020-11-27 12:19:02 +0300

    init

---
+++ 8-ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var api = new YouTubeApi();
+                var videos = api.GetVideos("ercan");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
